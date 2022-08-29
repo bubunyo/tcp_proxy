88da01486b8c86bffe867711c17d7c6b8d7cc0a4
Bubunyo Nyavor <samuel.bubunyo.nyavor@adjust.com>
2022-08-29 10:25:56 +0200

    update tcp proxy

--- proxy.go
+++ proxy.go
@@ -1,8 +1,9 @@
-package tcp_proxy
+package conn_proxy
 
 import (
 	"fmt"
 	"io"
+	"log"
 	"math/rand"
 	"net"
 	"os"
@@ -21,7 +22,6 @@ type Proxy struct {
 // a unix file socket. This allows you to manipulate the connection forwarding
 // however you please
 // and closes it when finished.
-// func New(raddr string) (*Proxy, error) {
 func New(raddr string) *Proxy {
 	p := &Proxy{
 		lSockFile: generateSockFile(),
@@ -29,23 +29,28 @@ func New(raddr string) *Proxy {
 	}
 	return p
 }
+
 func (p *Proxy) Addr() string {
 	return p.lSockFile
 }
+
 func (p *Proxy) logError(msg string, err error) {
-	fmt.Println("[Connection Proxy][Error]", msg, err.Error())
+	log.Print("[Connection Proxy][Error]", msg, err.Error())
 }
+
 func (p *Proxy) logMsg(msg ...interface{}) {
-	fmt.Print("[Connection Proxy] ")
-	fmt.Println(msg...)
+	log.Println("[Connection Proxy]", msg)
 }
+
 func (p *Proxy) Stop() {
 	p.logMsg("Intercepting and stopping connection")
 	close(p.stopChan)
 }
+
 func (p *Proxy) Start() {
 	go p.start()
 }
+
 func (p *Proxy) start() {
 	laddr, err := net.ResolveUnixAddr("unix", p.lSockFile)
 	if err != nil {
@@ -57,52 +62,66 @@ func (p *Proxy) start() {
 		p.logError("Resolve remote address error", err)
 		return
 	}
+
 	p.logMsg("Waiting for new connection")
+
 	listener, err := net.ListenUnix("unix", laddr)
 	if err != nil {
 		p.logError("Listen Unix Socket Error:", err)
 		return
 	}
+
 	listener.SetUnlinkOnClose(true)
+
 	p.stopChan = make(chan struct{})
+
 	for {
-		if listener == nil {
-			break
+		select {
+		case <-p.stopChan:
+			goto exit
+		default:
 		}
 		lconn, err := listener.AcceptUnix()
 		if err != nil {
 			p.logError("Accept unix connection error", err)
 			return
 		}
-		p.logMsg("New connection: ", lconn.RemoteAddr())
+		p.logMsg("New connection:", lconn.RemoteAddr())
+
 		go func() {
 			defer func() {
-				lconn.Close()
-				listener.Close()
-				listener = nil
+				_ = lconn.Close()
+				_ = listener.Close()
 			}()
+
 			// connect to remote
 			rconn, err := net.DialTCP("tcp", nil, raddr)
 			if err != nil {
-				p.logError("Remote connection failed: ", err)
+				p.logError("Remote connection failed:", err)
 				return
 			}
-			defer rconn.Close()
+			defer rconn.Close() //nolint:errcheck
+
 			// display both ends
 			p.logMsg("Opened:: Remote:", raddr.String(), "Local:", laddr.String())
+
 			closer := make(chan struct{})
+
 			// bidirectional copy
 			go p.pipe(closer, lconn, rconn)
 			go p.pipe(closer, rconn, lconn)
+
 			select {
 			case <-closer:
 				p.logMsg("Closing connection")
 			case <-p.stopChan:
 				p.logMsg("Stopping connection")
 			}
+
 			p.logMsg("Connection complete")
 		}()
 	}
+exit:
 	p.logMsg("Proxy shutting down")
 }
 
